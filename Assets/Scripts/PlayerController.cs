2e789edfdc6bfec1fac71dca2eb63aaa9bbe3db9
Self-Learning-AI <67587269+Self-Learning-AI@users.noreply.github.com>
2021-01-25 17:17:42 +0000

    Added jump to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 2.0f;
     private float vertical;     // For forwards/backwards movement
     private float horizontal;   // For left/right movement
+    private float jump;         // For jump input
     private bool canJump = true;
     private bool canMove = true;
     [Space]
@@ -37,32 +38,41 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // Fixed update is better when working with physics
+    // Called a set number of times per frame
     private void FixedUpdate()
     {
         Move();
+        Jump();
     }
 
     void Move()
     {
-        // Gets movement axis value from the input manager
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+        // Check if movement is allowed
+        if (canMove)
+        {
+            // Gets movement axis value from the input manager
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
-        // Adds force to make rigid body move
-        body.AddForce(movement * moveSpeed);
+            // Adds force to x and z axis to make rigid body move
+            body.AddForce(movement * moveSpeed);
+        }
+        
     }
 
     void Jump()
     {
+        // Check if allowed to jump first
+        if (canJump)
+        {
+            jump = Input.GetAxis("Jump");
+            Vector3 jumpVector = new Vector3(0.0f, jump, 0.0f);
 
+            // Adds force to x and z axis to make rigid body move
+            body.AddForce(jumpVector * jumpHeight);
+        }
     }
 
     public void TakeDamage(float incomingDamage)
@@ -100,5 +110,23 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
+    }
+
+    // Called when an object collider collides with this objects collider.
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            canJump = true;
+        }
+    }
+
+    // Called when an object collider is no longer colliding with this objects collider.
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            canJump = false;
+        }
     }
 }
